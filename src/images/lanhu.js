06c85d8e63816ac4ae162644ae06f7e5c9c4bb0a
吕群 <893969029@qq.com>
2020-04-21 00:58:06 +0800

    swiper图片显示有问题，pad菜单登录有问题，rem的运用

--- src/images/lanhu.js
+++ src/images/lanhu.js
@@ -35,17 +35,36 @@ $(document).ready(function() {
         $(document).on('scroll.unable', function(e) {
             $(document).scrollTop(top);
         })
-    }
+    };
     //移除禁止滚动条滚动
     function removeUnScroll() {
         $(document).unbind("scroll.unable");
-    }
+    };
 
+    // 点击模拟登录 start pc
+    // 声明一个变量，记录是否登录
+    var logintrue = false;
+    $('.header').on('click', '.js_denglu', function() {
+        $this = $(this);
+        $this.parents('.pcduan').addClass('displaynone');
+        $this.parents('.pcduan').siblings('.js_login_success').removeClass('displaynone');
+        logintrue = true;
+    });
+    // 点击模拟登录 end
+
+    // 点击模拟退出 start
+    $('.header').on('click', '.tuichu', function() {
+        $this = $(this);
+        $this.parents('.js_login_success').addClass('displaynone');
+        $this.parents('.js_login_success').siblings('.pcduan').removeClass('displaynone');
+        logintrue = false;
+
+    });
+    // 点击模拟退出 end
 
     // 评审团尺寸 start
     var jury_img_width = $('.jury .jury_img').width();
     $('.jury .intro').width(jury_img_width);
-    $('.jury .name').width(jury_img_width);
     // 评审团尺寸 end
 
     // 赛事动态尺寸 start
@@ -87,6 +106,7 @@ $(document).ready(function() {
     var width = $(window).width();
     var height = $(window).height();
     console.log(width);
+    console.log(height);
     if (width > 1200) {
         // 尾部二维码
         $('.js_weichat').on('mouseenter', function() {
@@ -99,7 +119,53 @@ $(document).ready(function() {
         });
 
 
-    } else if (width < 1200 && width > 750) {
+    } else if (width < 1200) {
+        // width<750
+        if (width <= 750) {
+            // 评审团 尺寸 start
+            var jury_item = $('.jury .jury_img').width();
+            console.log(jury_item);
+            $('.jury .intro').width(jury_item);
+            $('.jury .intro').css('padding', 0);
+            // 评审团 尺寸 end
+
+            // 手机端回到顶部 start
+            $('.goback').removeClass('displaynone');
+            $('.goback').on('click', function() {
+                $("html,body").animate({ scrollTop: 0 }, 500);
+            });
+            // 手机端回到顶部 end
+
+        }
+        // width>750&&width<1200
+        if (width > 750) {
+            // 评审团 不足一行三个 居中 start
+            var number = $('.jury .list').children('.item').length;
+            if (number % 3 == 2) {
+                $('.jury .list').find('.item').eq(number - 1).css('width', "50%");
+                $('.jury .list').find('.item').eq(number - 2).css('width', "50%");
+            } else if (number % 3 == 1) {
+                $('.jury .list').find('.item').eq(number - 1).css('width', "100%");
+            }
+            // 评审团 不足一行三个 居中 start
+
+
+        }
+
+        // 点击模拟登录 start pad and phone
+        $('.header').on('click', '.js_login_icon', function() {
+            logintrue = true;
+        });
+        // 点击模拟登录 end
+
+        // 点击模拟退出 start
+        $('.js_menu_zhezhao').on('click', '.tuichu', function() {
+            logintrue = false;
+            $this = $(this);
+            $this.parents('.login_success_pad_phone').addClass('displaynone');
+        });
+        // 点击模拟退出 end
+
         // 点击打开pad菜单栏
         $('.header').on('click', '.menu', function() {
             var $this = $(this);
@@ -111,6 +177,11 @@ $(document).ready(function() {
                 $zhezhao.removeClass('displaynone');
                 $this.siblings('.js_close_menu').removeClass('displaynone');
                 $('body').css({ overflow: 'hidden', height: "100%" });
+                if (logintrue) {
+                    $('.login_success_pad_phone').removeClass('displaynone');
+                    var login_height = $('.js_last_menu_item').offset().top + 2 * ($('.js_last_menu_item').height());
+                    $('.login_success_pad_phone').css('top', login_height);
+                }
             }
         });
         //点击关闭pad菜单栏
@@ -120,11 +191,29 @@ $(document).ready(function() {
             $('.js_menu_zhezhao').addClass('displaynone');
             $('body').css('overflow', 'auto');
         });
-    } else if (width <= 750) {
-        var jury_item = $('.jury .jury_img').width();
-        console.log(jury_item);
-        $('.jury .intro').width(jury_item);
-        $('.jury .name').width(jury_item);
-        $('.jury .intro').css('padding', 0);
+    }
+});
+
+
+// 窗口函数改变执行的
+$(window).resize(function() {
+    var widthnow = $(window).width();
+    if (widthnow < 1200) {
+        // 菜单栏顶部 start
+        var headwidth = $('.header').height();
+        var $zhezhao = $('.js_menu_zhezhao');
+        $zhezhao.css('top', headwidth);
+        // 菜单栏顶部 end
+
+        // 赛事动态尺寸 start
+        var dynamic_img_width = $('.dynamic .top img').width();
+        $('.dynamic .left').width(dynamic_img_width);
+        // 赛事动态尺寸 end
+
+        // 评审团尺寸 start
+        var jury_img_width = $('.jury .jury_img').width();
+        $('.jury .intro').width(jury_img_width);
+        // 评审团尺寸 end
+
     }
 });
